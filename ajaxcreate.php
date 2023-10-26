f58495ba087197f1b967b8e8dd13d9b48f676c03
RifqiMuafa20 <muhammadrifqi20001@gmail.com>
2023-10-26 15:58:20 +0800

    feature: fetch, create, delete

---
+++ ajaxcreate.php
@@ -0,0 +1,35 @@
+<?php
+
+include "config.php";
+
+$data = json_decode(file_get_contents("php://input"));
+
+$request = $data->request;
+
+if($request == 2){
+    $name = $data->name;
+    $addres = $data->addres;
+    $contact = $data->contact;
+    $lapangan = $data->lapangan;
+    $durasi = $data->durasi;
+    $date = $data->date;
+    $time = $data->time;
+
+    mysqli_query($con, "INSERT INTO pemesanan(name, addres, contact, lapangan, durasi, date, time) VALUES('".$name."', '".$addres."', '".$contact."', '".$lapangan."', '".$durasi."', '".$date."', '".$time."')");
+    echo "Success";
+}
+
+// if($request == 3){
+//     $id = $data->id;
+//     $name = $data->name;
+//     $addres = $data->addres;
+//     $contact = $data->contact;
+//     $lapangan = $data->lapangan;
+//     $durasi = $data->durasi;
+//     $date = $data->date;
+//     $time = $data->time;
+
+//     mysqli_query($con, "UPDATE pemesanan SET name='".$name."', addres='".$addres."', contact='".$contact."', lapangan='".$lapangan."', durasi='".$durasi."', date='".$date."', time='".$time."' WHERE id=".$id);
+//     echo "Success";
+//     exit;
+// }
